c412dedb7cc8fc0f5fbcac89d802ad21d906fce1
kaustav1616 <kaustav1616@yahoo.com>
2021-05-31 13:18:26 +0530

    added singleton design pattern

--- Design_patterns/Singleton.java
+++ Design_patterns/Singleton.java
@@ -1,9 +1,16 @@
+/* idea is to make it so that object of Singleton can only be created from inside the class, 
+ * and therefore we can control how many objects of the class can be created. Since we cannot
+ * create an object externally, we need a static method of the Singleton class that can
+ * return an object of it that has been created from inside the class.
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 
 class Singleton
 {
-    static private Singleton obj = new Singleton();
+    // static variable because has to be accessible from static method getInstance()
+	static private Singleton obj = new Singleton();
     
     private Singleton()
     {
